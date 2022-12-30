d9721ae013edff8610412cb4504c396c6b6441a4
Sumer Cetin <sumercetin3@gmail.com>
2022-12-30 23:21:52 +0300

    PersonalWebsite :: Add Link Section

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Image from 'next/image';
 import HomeLayout from '../components/homelayout';
 import homeStyles from '../styles/home.module.css';
@@ -10,6 +11,7 @@ export default function Home() {
       <Head>
         <title>Sümer Çetin</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/images/sumercetin.svg"></link>
       </Head>
       <IntroSection />
     </HomeLayout>
@@ -18,18 +20,31 @@ export default function Home() {
 
 const IntroSection = () => {
   return (
-    <div className={`${utilStyles.row} ${homeStyles.intro}`}>
-      <Image
-        className={`${homeStyles.logo}`}
-        src="/images/sumercetin.svg"
-        height={120}
-        width={120}
-        alt="Sümer Çetin"
-      />
-      <div className={`${utilStyles.col} ${homeStyles.introTextContainer}`}>
-        <span className={`${homeStyles.name}`}>Sümer Çetin</span>
-        <span className={`${homeStyles.title}`}>Frontend Developer</span>
+    <>
+      <div className={`${utilStyles.row} ${homeStyles.intro}`}>
+        <Image
+          className={`${homeStyles.logo}`}
+          src="/images/sumercetin.svg"
+          height={120}
+          width={120}
+          alt="Sümer Çetin"
+        />
+        <div className={`${utilStyles.col} ${homeStyles.introTextContainer}`}>
+          <span className={`${homeStyles.name}`}>Sümer Çetin</span>
+          <span className={`${homeStyles.title}`}>Frontend Developer</span>
+        </div>
       </div>
-    </div>
+      <LinkSection />
+    </>
   );
-}
+};
+
+const LinkSection = () => {
+  return (
+    <nav className={`${utilStyles.row} ${homeStyles.linkContainer}`}>
+      <Link href="#">About</Link>
+      <Link href="#">Work</Link>
+      <Link href="#">Projects</Link>
+    </nav>
+  );
+};
